6205a0c1418850d48d9db7c2e411f049103b6d8a
Jardiel Freitas Cunha <jfcjardiel@gmail.com>
2021-08-26 21:19:09 -0300

    Going further with the website

--- PROJECTS/web_applications/learning_logs/models.py
+++ PROJECTS/web_applications/learning_logs/models.py
@@ -1,4 +1,6 @@
+from datetime import datetime , timedelta
 from django.db import models
+from django.utils import timezone
 
 #Here we are going to build the data models of our app
 #The each one of the data is a class
@@ -10,6 +12,9 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
     def __str__(self):
         """Return a string representation of the model"""
         return self.text
@@ -20,6 +25,9 @@ class Entry(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE, null=True)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
     class Meta:
         verbose_name_plural = 'entries'
--- PROJECTS/web_applications/learning_logs/views.py
+++ PROJECTS/web_applications/learning_logs/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Topic, Entry
+
 # Create your views here.
+
+#creating the first views!
+def index(request):
+    latest_topic_list = Topic.objects.order_by('-date_added')[:5]
+    template = loader.get_template('learning_logs/index.html')
+    context = {
+        'latest_topic_list': latest_topic_list
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
