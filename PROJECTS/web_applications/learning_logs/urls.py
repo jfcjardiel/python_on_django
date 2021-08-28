77683df03ccf453b246df751b78c42b91a532a68
Jardiel Freitas Cunha <jfcjardiel@gmail.com>
2021-08-28 02:40:14 -0300

    Adding urls and templates of the page

--- PROJECTS/web_applications/learning_logs/urls.py
+++ PROJECTS/web_applications/learning_logs/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'learning_logs'
 urlpatterns = [
     path('', views.index, name='index'),
     # ex: /learning_logs/5
--- PROJECTS/web_applications/learning_logs/views.py
+++ PROJECTS/web_applications/learning_logs/views.py
@@ -1,23 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
 
 from .models import Topic, Entry
 
 # Create your views here.
 
-#creating the first views!
+#show all the topics which were created
 def index(request):
     latest_topic_list = Topic.objects.order_by('-date_added')[:5]
-    template = loader.get_template('learning_logs/index.html')
     context = {
         'latest_topic_list': latest_topic_list
     }
-    return HttpResponse(template.render(context, request))
+    #we use render rather than: 
+    # template = loader.get_template('polls/index.html')
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'learning_logs/index.html', context)
 
+#exibits the details of the topics
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    #using get_object_or_404
+    question = get_object_or_404(Topic, id=question_id)
+    entry = get_list_or_404(Entry, topic=question_id)
+    return render(request, 'learning_logs/details.html', {'question': question, 'entry': entry})
+#    #another option
+#    try:
+#        question = Topic.objects.get(id=question_id)
+#    except Topic.DoesNotExist:
+#        raise Http404("Question does not exist")
+#    return render(request, 'learning_logs/details.html', {'question': question})
+#    
+#   #using just HttpResponse
+#   return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
