2d2003a0c2d8d6a640041def099ff21cabcc0cd1
Jardiel Freitas Cunha <jfcjardiel@gmail.com>
2022-03-01 16:04:56 -0300

    Creating webscrapping for passport

--- PROJECTS/web_applications/learning_log/urls.py
+++ PROJECTS/web_applications/learning_log/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('learning_logs/', include('learning_logs.urls')),
+    path('polls/', include('polls.urls')),
 ]
